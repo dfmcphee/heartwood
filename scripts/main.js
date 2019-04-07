39f6580178edaa5876a324fbce92d3a607490a03
Dominic McPhee <dominic.mcphee@gmail.com>
2019-04-07 12:26:54 -0400

    More update to large screen site

---
+++ scripts/main.js
@@ -0,0 +1,10 @@
+var feed = new Instafeed({
+	get: 'user',
+	userId: '245809',
+	accessToken: '245809.f6bcbd5.3d33933ce4fa4aa6938f633a3292fad3',
+	template: '<a href="{{link}}"><img src="{{image}}" /></a>',
+	error: function(error) {
+		console.log(error);
+	}
+});
+//feed.run();
