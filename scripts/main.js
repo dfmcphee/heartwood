092a609be5f3d69870ed98cd9b180f515df59466
Dominic McPhee <dominic.mcphee@gmail.com>
2019-04-08 00:11:58 -0400

    Reorganizing styles and cleaning up markup

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,14 @@
-var feed = new Instafeed({
-	get: 'user',
-	userId: '245809',
-	accessToken: '245809.f6bcbd5.3d33933ce4fa4aa6938f633a3292fad3',
-	template: '<a href="{{link}}"><img src="{{image}}" /></a>',
-	error: function(error) {
-		console.log(error);
-	}
-});
-//feed.run();
+document.addEventListener('DOMContentLoaded', function() {
+	svg4everybody();
+	
+	var feed = new Instafeed({
+		get: 'user',
+		userId: '245809',
+		accessToken: '245809.f6bcbd5.3d33933ce4fa4aa6938f633a3292fad3',
+		template: '<a href="{{link}}"><img src="{{image}}" /></a>',
+		error: function(error) {
+			console.log(error);
+		}
+	});
+	//feed.run();
+}, false);
